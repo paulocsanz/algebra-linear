3fc3d6bec14919efdb3efc3e6b810230ea9104df
Paulo Cabral Sanz <paulocabral@sanz.com.br>
2017-05-28 05:18:24 -0300

    Answer more questions of list 4

---
+++ lista4/lista4.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python3
+
+from math import log, cosh, tanh, sqrt, cos, sin, e
+from erros import NaoConverge
+from bissecao import raiz as raiz_bissecao
+from newton import raiz as raiz_newton
+from secante import raiz as raiz_secante
+from interpolacao_inversa import raiz as raiz_interpolacao
+from eq_n_linear_newton import SolucaoEqNLinearNewton
+from eq_n_linear_broyden import SolucaoEqNLinearBroyden
+
+print("Questão 1:")
+g = 9.806
+k = 0.00341
+c = sqrt(g * k)
+f = lambda x: log(cosh(x * c)) - 50
+df = lambda x: g * tanh(x * c)
+
+print("1) Raiz por bisseção:\n", raiz_bissecao(0, 1000, 0.0001, f))
+
+try:
+    print("2) Raiz por Newton original:\n", raiz_newton(1000, 5, f, df, 0.0001))
+except ZeroDivisionError as ex:
+    print("2) O resultado da derivada da função para x = {} é zero, "
+          "portanto ocorre uma divisão por zero durante a resolução pelo método de Newton.".format(str(ex)))
+except NaoConverge:
+    print("2) Raiz por Newton original não converge")
+
+try:
+    print("3) Raiz por Newton secante:\n", raiz_secante(1000, 5, f, 0.0001))
+except NaoConverge:
+    print("3) Raiz por Newton secante não converge")
+
+#try:
+#    print("4) Raiz por interpolação inversa:\n", raiz_interpolacao(f, 0, 1, 2, 1000, 0.0001))
+#except NaoConverge:
+#    print("4) Raiz por interpolacao inversa não converge")
+
+print("\nQuestão 2:")
+f = lambda x: 4 * cos(x) - e**(2*x)
+df = lambda x: -2 * (e**(2*x) + 2 * sin(x))
+
+print("1) Raiz por bisseção:\n", raiz_bissecao(0, 15, 0.0001, f))
+
+try:
+    print("2) Raiz por Newton original:\n", raiz_newton(1000, 5, f, df, 0.0001))
+except ZeroDivisionError as ex:
+    print("2) O resultado da derivada da função para x = {} é zero, "
+          "portanto ocorre uma divisão por zero durante a resolução pelo método de Newton.".format(str(ex)))
+except NaoConverge:
+    print("2) Raiz por Newton não converge")
+
+try:
+    print("3) Raiz por Newton secante:\n", raiz_secante(1000, 10, f, 0.0001))
+except NaoConverge:
+    print("3) Raiz por secante não converge")
+
+try:
+    print("4) Raiz por interpolação inversa:\n", raiz_interpolacao(f, 5, 10, 15, 1000, 0.0001))
+except NaoConverge:
+    print("4) Raiz por interpolacao inversa não converge")
+
+print("\nQuestão 3:")
+
+F = lambda x: [[16 * x[0]**4 + 16 * x[1]**4 + x[2]**4 - 16],
+               [x[0]*x[0] + x[1]*x[1] + x[2]*x[2] - 3],
+               [x[0]**3 - x[1] + x[2] - 1]]
+J = lambda x: [[64 * x[0]**3, 64 * x[1]**3, 4 * x[2]**3],
+               [2*x[0], 2*x[1], 2*x[2]],
+               [3 * x[0]*x[0], -1, 1]]
+try:
+    print("1) Newton N equações:", SolucaoEqNLinearNewton([0.5, 1, 2], 0.0001, 1000, J, F))
+except NaoConverge:
+    print("1) Método por Newton para N equações não converge")
+
+J = 3 #jacobiana de partida ??
+#try:
+#    print("2) Método de Broyden:", SolucaoEqNLinearBroyden([0.5, 1, 2], J, 0.0001, 1000, F))
+#except NaoConverge:
+#    print("2) Método de Broyden não converge")
+
+print("\nQuestão 4:")
+theta1, theta2 = 0, 3
+F = lambda x: [[2 * x[1]*x[1] + x[0]*x[0] + 6 * x[2]*x[2] - 1],
+               [8 * x[1]**3 + 6 * x[1] * x[0]*x[0] + 36 * x[1] * x[0] * x[2] + 108 * x[1] * x[2]*x[2] - theta1],
+               [60 * x[1]**4 + 60 * x[1]*x[1] * x[0]*x[0] + 576 * x[1]*x[1] * x[0] * x[2] + 2232 * x[1]*x[1] * x[2]*x[2] + 252 * x[2]*x[2] * x[0]*x[0] + 1296 * x[2]**3 * x[0] + 3348 * x[2]**4 + 24 * x[0]**3 * x[2] + 3 * x[0] - theta2]]
+
+J = lambda x: [[2 * x[0], 4 * x[1], 2 * x[2]],
+               [12 * x[0], 24 * x[1]*x[1] + 6 + 36 + 108, 36 + 216 * x[2]],
+               [120 * x[0] + 576 + 504 * x[0] + 1296 + 3348 * 3 * x[0]*x[0], 240 * x[1]**3 + 120 * x[1] + 1154 * x[1] + 4464 * x[1], 576 + 4464 * x[2] + 504 * x[2] + 1296 * 3 * x[2]*x[2] + 3348 * 4 * x[2]**3 + 24]]
+
+print("a)")
+try:
+    print("Newton:", SolucaoEqNLinearNewton([0, 0.01, 0.02], 0.00001, 1000, J, F))
+except NaoConverge:
+    print("Método de Newton não converge")
