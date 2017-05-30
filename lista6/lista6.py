b7b62a773249f46fee0f0451416a47f11b11fec2
Paulo Cabral Sanz <paulocabral@sanz.com.br>
2017-05-29 23:57:41 -0300

    Plot graphs

--- lista6/lista6.py
+++ lista6/lista6.py
@@ -1,9 +1,26 @@
+import matplotlib.pyplot as plt
+from threading import Thread
 from euler import EulerEDO
 from runge_kutta_2a import RungeKutta2aEDO
 from runge_kutta_4a import RungeKutta4aEDO
 from runge_kutta_nystrom_edo2 import RungeKuttaNystromEDO2
 from serie_taylor_edo2 import TaylorEDO2
 from math import e, pi, log, sqrt, cos, sin
+
+def plot(array, title, color='blue'):
+    fig = plt.figure(figsize=(8, 6), dpi=80)
+    ax = plt.subplot(111)
+    plt.plot(range(len(array)),
+             array,
+             color=color,
+             linewidth=1.0,
+             linestyle="-")
+    ax.set_title(title)
+    plt.grid(True)
+    plt.ylabel('x(t)')
+    plt.xlabel('s')
+
+    fig.show()
 
 print("Exercício 1")
 f = lambda x, t: -2*t*(x**2)
@@ -30,20 +47,20 @@ c = 0.2
 k = 1
 w = 0.5
 f = lambda x, dx, t: 2*sin(w*t) + sin(2*w*t) + cos(3*w*t) - c*dx - k*x
-resRKN = RungeKuttaNystromEDO2 (0, 0, 0, 100, 0.1, f)
-resT = TaylorEDO2 (0, 0, 0, 100, 0.1, f)
+runge_kutta = RungeKuttaNystromEDO2 (0, 0, 0, 100, 0.1, f)
+taylor = TaylorEDO2 (0, 0, 0, 100, 0.1, f)
 
-for i in range(0,10): #TA AQUI SO PA EU TESTA AS PARADA MAL PODE COMENTA
-	print(resRKN[i], " - ", resT[i])
-
-
-
+plot(runge_kutta, "Exercício 2: Método Runge-Kutta")
+plot(taylor, "Exercício 2: Método de Taylor", color='red')
 
 print("\n\nExercício 3")
 
 f = lambda x, dx, t: -9.807-1*dx*abs(dx)
-resRKN2 = RungeKuttaNystromEDO2(0, 0, 0, 20, 0.1, f)
-resT2 = TaylorEDO2(0, 0, 0, 20, 0.1, f)
+runge_kutta = RungeKuttaNystromEDO2(0, 0, 0, 20, 0.1, f)
+taylor = TaylorEDO2(0, 0, 0, 20, 0.1, f)
 
-for i in range(0,10): #TA AQUI SO PA EU TESTA AS PARADA MAL PODE COMENTA
-	print(resRKN2[i], " - ", resT2[i])
+plot(runge_kutta, "Exercício 3: Método Runge-Kutta")
+plot(taylor, "Exercício 3: Método de Taylor", color='red')
+
+# Ultima coisa no script (bloqueia até os gráficos fecharem)
+plt.show()
